3ef3d6dba393861054eb702eb4d122c9a9c442b5
Chhy Riden <chhe.ridan@gmail.com>
2018-03-15 00:31:50 +0700

    edited welcome file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laraden
+                    Laravel
                 </div>
             </div>
         </div>
